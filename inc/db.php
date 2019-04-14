72cd3909e0efdd5e4b37279002fba933d5756195
dlindgren86 <dennis.lindgren@gmail.com>
2019-04-14 17:15:56 +0200

    CMS Project

---
+++ inc/db.php
@@ -0,0 +1,13 @@
+<?php
+    define("DB_HOST", 'localhost');
+    define("DB_USER", 'root');
+    define("DB_PASS", 'hej123');
+    define("DB_NAME", 'cms');
+
+    $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+  /*  if($connection){
+        echo 'we are connected';
+    }
+*/
+?>
---
+++ index.php
@@ -0,0 +1,64 @@
+<?php 
+
+include('inc/db.php');
+include('inc/header.php');
+
+?>
+    <!-- Navigation -->
+    <?php include('inc/navbar.php'); ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">
+
+                <?php
+                    $query = 'SELECT * FROM posts';
+                    $postQuery = mysqli_query($connection, $query);
+                    
+                    while($row = mysqli_fetch_assoc($postQuery)){
+                        $post_title = $row['post_title'];
+                        $post_content = $row['post_content'];
+                        $post_author = $row['post_author'];
+                        $post_date = $row['post_date'];
+                        $post_img = $row['post_img'];
+
+                        ?>
+
+                    <h1 class="page-header">
+                        Page Heading
+                        <small>Secondary Text</small>
+                    </h1>
+
+                    <!-- First Blog Post -->
+                    <h2>
+                        <a href="#"><?php echo $post_title; ?></a>
+                    </h2>
+                    <p class="lead">
+                        by <a href="index.php"><?php echo $post_author; ?></a>
+                    </p>
+                    <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?></p>
+                    <hr>
+                    <img class="img-responsive" src="img/<?php echo $post_img; ?>" alt="">
+                    <hr>
+                    <p><?php echo $post_content; ?></p>
+                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                    <hr>
+             <?php }
+                ?>
+
+
+            </div>
+
+            <!-- Blog Sidebar Widgets Column -->
+            <?php include('inc/sidebar.php'); ?>
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+<?php include('inc/footer.php'); ?>
